216dffa3c83a5f29dc24f4339ec41484480ffd3d
Khitoshi <kubotah1115@gmail.com>
2025-05-31 09:47:51 +0000

    Add deployment scripts to package.json and set base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : '/Github-Pages-Tutorial/',
   server: {
     host: true, // すべてのアドレスからのアクセスを許可
     port: 5173
